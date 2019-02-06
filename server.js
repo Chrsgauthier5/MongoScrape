7014152daf4ad9a9d94bcb791655f5a2ec76331f
Chris Gauthier <chrsgauthier5@gmail.com>
2019-02-05 20:41:55 -0500

    fix port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cheerio = require("cheerio"); // scraping
 // Require all models
 const db = require("./models");
 
-const PORT = 3000;
+const PORT =  porcess.env.PORT || 3000;
 
 // Initialize Express
 const app = express();
